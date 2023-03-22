e1246b29846576b4b9718731f4af0ac761981a3b
Thanasis Kyprianos <than20022004@gmail.com>
2023-03-22 23:14:15 +0200

    Fixed bug

--- blocks.h
+++ blocks.h
@@ -6,4 +6,4 @@ void add_block_state(char *name, char *on, blocks state);
 int is_on(char *name, char *on, blocks state);
 int holds(char *name, blocks state);
 void print_blocks(blocks state);
-int start_goal(blocks state);
+int start_goal(FILE *fptr, int block_count, blocks state);
